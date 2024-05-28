4fff60250edb605d5bea069b211a953405fb4a20
VamsiPriya-Vemulamada <vamsipiryavemulamada@outlook.com>
2024-05-28 14:55:53 -0400

    Day1 only with datatypes

---
+++ Datatypes.py
@@ -0,0 +1,29 @@
+# integer
+
+
+a = 5
+print("printing the integer", a);
+
+b = 5.5
+print("printing the float number", b);
+
+# Integer, Float, Boolean, String, List, Tuple, Dictionary, Sets,
+
+Int_Var = 5
+Float_Var = 5.5
+String_Var = "This is priya"
+Boolean_Var = True  ###
+# There are number of points to know about the boolean data type, function and operators checkout in geeks
+List_Var = [1, 2, 3, 4]
+Tuple_Var = (1, 2, 2, 3)
+Set_Var = {1, 2, 3, 4}
+Dictionary_Var = {'1': 0, '2': 1, '3': 3}
+# printing the above variables
+print("Type integer", type(Int_Var), "Type float", type(Float_Var), "Type String", type(String_Var), "Type Boolean",
+      type(Boolean_Var), )
+
+# the above statement gives the class type of each variable
+print("Type integer", Int_Var, '\n' "Type float", Float_Var, '\n' "Type String", String_Var, '\n' "Type Boolean",
+      Boolean_Var)
+
+# Operators using the functions
